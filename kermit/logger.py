8c0f3548516092aaefea10f58dc4720e63ee6921
Ting Kai Chiu <tingkai.chiu@gmail.com>
2024-09-20 14:24:12 +0800

    Implemented logger and add it to gitignore

--- kermit/logger.py
+++ kermit/logger.py
@@ -3,7 +3,7 @@ from datetime import datetime
 
 from loguru import logger as _logger
 
-from metagpt.const import METAGPT_ROOT
+from kermit.constants import PACKAGE_ROOT
 
 _print_level = "INFO"
 
@@ -14,12 +14,12 @@ def define_log_level(print_level="INFO", logfile_level="DEBUG", name: str = None
     _print_level = print_level
 
     current_date = datetime.now()
-    formatted_date = current_date.strftime("%Y%m%d")
+    formatted_date = current_date.strftime("%Y-%m-%d")
     log_name = f"{name}_{formatted_date}" if name else formatted_date  # name a log with prefix name
 
     _logger.remove()
     _logger.add(sys.stderr, level=print_level)
-    _logger.add(METAGPT_ROOT / f"logs/{log_name}.txt", level=logfile_level)
+    _logger.add(PACKAGE_ROOT / f"logs/{log_name}.txt", level=logfile_level)
     return _logger
 
 
